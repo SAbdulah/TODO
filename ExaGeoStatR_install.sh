06e3e4bf3812bae28e304ac880dca40612cf9aee
Sameh Abdulah <23186151+SAbdulah@users.noreply.github.com>
2018-10-08 09:53:04 +0300

    Create ExaGeoStatR_install.sh

---
+++ ExaGeoStatR_install.sh
@@ -0,0 +1,56 @@
+module load mkl/2018-initial
+module load gcc/5.5.0
+module load cmake/3.9.6
+module load hwloc/1.11.8-gcc-5.5.0
+module load openmpi/3.0.0-gcc-5.5.0
+module load starpu/1.2.3-gcc-5.5.0-mkl-openmpi-3.0.0
+module load gsl/2.4-gcc-5.5.0
+module load nlopt/2.4.2-gcc-5.5.0
+module load r/3.2.3
+
+rm -rf exageostatR-dev
+git clone https://github.com/ecrc/exageostatR-dev.git
+cd exageostatR-dev/
+git checkout sabdulah/gen-locs-with-seed
+git submodule update --init --recursive
+export EXAGEOSTATDEVDIR=$PWD/src
+cd $EXAGEOSTATDEVDIR
+export HICMADIR=$EXAGEOSTATDEVDIR/hicma-dev
+export CHAMELEONDIR=$EXAGEOSTATDEVDIR/hicma-dev/chameleon
+export STARSHDIR=$EXAGEOSTATDEVDIR/stars-h-dev
+
+## STARS-H
+cd $STARSHDIR
+rm -rf build
+mkdir -p build
+cd build
+cmake .. -DMPI=OFF -DOPENMP=OFF -DSTARPU=OFF -DCMAKE_C_FLAGS="-fPIC"
+make -j
+make install
+
+## CHAMELEON
+cd $CHAMELEONDIR
+rm -rf build
+mkdir -p build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCHAMELEON_USE_MPI=OFF -DCHAMELEON_ENABLE_EXAMPLE=OFF -DCHAMELEON_ENABLE_TESTING=OFF -DCHAMELEON_ENABLE_TIMING=OFF -DBUILD_SHARED_LIBS:BOOL=ON # -DCMAKE_C_FLAGS="-fPIC"
+make -j # CHAMELEON parallel build seems to be fixed
+make install
+
+## HICMA
+cd $HICMADIR
+rm -rf build
+mkdir -p build
+cd build
+cmake .. -DHICMA_USE_MPI=OFF -DHICMA_ENABLE_TESTING=OFF -DHICMA_ENABLE_TIMING=OFF -DBUILD_SHARED_LIBS:BOOL=ON # -DCMAKE_C_FLAGS="-fPIC"
+make -j
+make install
+
+cd $EXAGEOSTATDEVDIR/..
+export CPATH=$CPATH:/usr/local/include/coreblas && \
+#export LD_LIBRARY_PATH="${MKLROOT}/lib/intel64_lin:$LD_LIBRARY_PATH" && \
+export LIBRARY_PATH="$LD_LIBRARY_PATH"
+
+## Modify src/Makefile, compilation flagss -> flagsl
+R CMD build . && \
+R CMD INSTALL ./exageostat_0.1.0.tar.gz
