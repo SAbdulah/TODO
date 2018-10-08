2eef4fbc79aa278e321995e87562d7790fea87a3
sabdulah <sameh.abdulah@kaust.edu.sa>
2018-10-08 12:50:08 +0300

    major

--- ExaGeoStatR_install.sh
+++ ExaGeoStatR_install.sh
@@ -1,15 +1,34 @@
-module load mkl/2018-initial
-module load gcc/5.5.0
-module load cmake/3.9.6
-module load hwloc/1.11.8-gcc-5.5.0
-module load openmpi/3.0.0-gcc-5.5.0
-module load starpu/1.2.3-gcc-5.5.0-mkl-openmpi-3.0.0
-module load gsl/2.4-gcc-5.5.0
-module load nlopt/2.4.2-gcc-5.5.0
-module load r/3.2.3
+module load ecrc-extras
+module load mkl/2018-update-1
+module load gcc/7.2.0
+module load cmake/3.11.1
+module load gsl/2.4-gcc-7.2.0
+module load nlopt/2.4.2-gcc-7.2.0
+module load hwloc/1.11.8-gcc-7.2.0
+module load starpu/1.2.4-gcc-7.2.0-mkl-openmpi-3.0.0
+module load hdf5/1.10.1-gcc-7.2.0
+module load netcdf/4.5.0-gcc-7.2.0
+
+sed -i '/## EXAGEOSTAT-INSTALLATION-BEGIN/,/## EXAGEOSTAT-INSTALLATION-END/d'  ~/.bashrc
+echo '## EXAGEOSTAT-INSTALLATION-BEGIN' >> ~/.bashrc
+
+
+echo 'module load ecrc-extras' >> ~/.bashrc
+echo 'module load mkl/2018-update-1' >> ~/.bashrc
+echo 'module load gcc/7.2.0' >> ~/.bashrc
+echo 'module load cmake/3.11.1' >> ~/.bashrc
+echo 'module load gsl/2.4-gcc-7.2.0' >> ~/.bashrc
+echo 'module load nlopt/2.4.2-gcc-7.2.0' >> ~/.bashrc
+echo 'module load hwloc/1.11.8-gcc-7.2.0' >> ~/.bashrc
+echo 'module load starpu/1.2.4-gcc-7.2.0-mkl-openmpi-3.0.0' >> ~/.bashrc
+echo 'module load hdf5/1.10.1-gcc-7.2.0' >> ~/.bashrc
+echo 'module load netcdf/4.5.0-gcc-7.2.0' >> ~/.bashrc
+
+
 
 rm -rf exageostatR-dev
 git clone https://github.com/ecrc/exageostatR-dev.git
+export DIR=$PWD
 cd exageostatR-dev/
 git checkout sabdulah/gen-locs-with-seed
 git submodule update --init --recursive
@@ -29,6 +48,7 @@ make -j
 make install
 
 export PKG_CONFIG_PATH=$STARSHDIR/build/lib/pkgconfig:$PKG_CONFIG_PATH
+echo 'PKG_CONFIG_PATH='$STARSHDIR'/build/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.bashrc
 
 ## CHAMELEON
 cd $CHAMELEONDIR
@@ -40,7 +60,7 @@ make -j # CHAMELEON parallel build seems to be fixed
 make install
 
 export PKG_CONFIG_PATH=$CHAMELEONDIR/build/lib/pkgconfig:$PKG_CONFIG_PATH
-
+echo 'export PKG_CONFIG_PATH='$CHAMELEONDIR'/build/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.bashrc
 
 ## HICMA
 cd $HICMADIR
@@ -52,15 +72,19 @@ make -j
 make install
 
 export PKG_CONFIG_PATH=$HICMADIR/build/lib/pkgconfig:$PKG_CONFIG_PATH
+echo 'export PKG_CONFIG_PATH='$HICMADIR'/build/lib/pkgconfig:$PKG_CONFIG_PATH' >> ~/.bashrc
 
+echo '## EXAGEOSTAT-INSTALLATION-END' >> ~/.bashrc
 
-
-cd $EXAGEOSTATDEVDIR/..
-export CPATH=$CPATH:/usr/local/include/coreblas && \
+cd $DIR
+#export CPATH=$CPATH:/usr/local/include/coreblas && \
 #export LD_LIBRARY_PATH="${MKLROOT}/lib/intel64_lin:$LD_LIBRARY_PATH" && \
-export LIBRARY_PATH="$LD_LIBRARY_PATH"
+#export LIBRARY_PATH="$LD_LIBRARY_PATH"
+echo $PWD
 
 ## Modify src/Makefile, compilation flagss -> flagsl
 R CMD build exageostatR-dev
-mkdir R-libraries
-R CMD INSTALL --library=./R-libraries ./exageostat_0.1.0.tar.gz
+
+#mkdir R-libraries
+#R CMD INSTALL --library=./R-libraries --repos="http://cran.us.r-project.org"  RhpcBLASctl
+#R CMD INSTALL --library=./R-libraries ./exageostat_0.1.0.tar.gz
---
+++ test2.R
@@ -0,0 +1,35 @@
+install.packages(lib="./R-libraries", repos="http://cran.us.r-project.org", "RhpcBLASctl")
+install.packages(lib="./R-libraries", "exageostat_0.1.0.tar.gz")
+library("exageostat")                                           #Load ExaGeoStat-R lib.
+seed            = 0                                             #Initial seed to generate XY locs.
+theta1          = 1                                             #Initial variance.
+theta2          = 0.1                                          #Initial range.
+theta3          = 0.5                                           #Initial smoothness.
+dmetric         = 0                                             #0 --> Euclidean distance, 1--> great circle distance.
+n               = 144                                           #n*n locations grid.
+ncores          = 2                                             #Number of underlying CPUs.
+gpus            = 0                                             #Number of underlying GPUs.
+dts             = 320                                           #Tile_size:  changing it can improve the performance. No fixed value can be given.
+lts             = 144                                           #TLR_Tile_size:  changing it can improve the performance. No fixed value can be given.
+p_grid          = 1                                             #More than 1 in the case of distributed systems.
+q_grid          = 1                                             #More than 1 in the case of distributed systems ( usually equals to p_grid).
+clb             = vector(mode="double", length = 3)            #Optimization function lower bounds values.
+cub             = vector(mode="double", length = 3)            #Optimization function upper bounds values.
+theta_out       = vector(mode="double", length = 3)            #Parameter vector output.
+globalveclen    = 3*n
+vecs_out        = vector(mode="double", length = globalveclen) #Z measurements of n locations.
+clb             = as.double(c("0.01", "0.01", "0.01"))         #Optimization lower bounds.
+cub             = as.double(c("5.00", "5.00", "5.00"))         #Optimization upper bounds.
+tlr_acc         = 12                                             #Approximation accuracy 10^-(acc)
+tlr_maxrank     = 450                                           #Max Rank
+vecs_out[1:globalveclen]        = -1.99
+theta_out[1:3]                  = -1.99
+#Initiate exageostat instance
+exageostat_initR(ncores, gpus, dts)
+#Generate Z observation vector
+vecs_out        = exageostat_egenzR(n, ncores, gpus, dts, p_grid, q_grid, theta1, theta2, theta3, dmetric, seed, globalveclen)
+#Estimate MLE parameters (TLR approximation)
+theta_out       = exageostat_tlrmleR(n, ncores, gpus, lts, p_grid, q_grid,  vecs_out[1:n],  vecs_out[n+1:(2*n)],  vecs_out[(2*n+1):(3*n)], clb, cub, tlr_acc, tlr_maxrank,  dmetric, 0.0001, 100000
+                                  )
+#Finalize exageostat instance
+exageostat_finalizeR()
